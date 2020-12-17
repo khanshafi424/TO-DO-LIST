165d72b5695aea08892824837cd2c7dc553b53b7
shafi <khanshafi424@gmail.com>
2020-12-17 15:38:30 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import  {Todo} from './todo'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,61 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Practice';
+
+  title = 'ToDO List';
+  todos: Todo[] = [] ;
+  name =""
+  editable = false;
+  addButton='+';
+  editableIndex = 0;
+  isChecked =  false;
+  alert = false;
+
+  
+ add() {
+
+  if(this.editable === true){
+   const todosList : Todo = {
+      name : this.name,
+      editable : this.editable
+   }
+   this.todos.splice(this.editableIndex ,1,todosList);
+   this.addButton='+';
+   this.editable = false;
+   this.name='';
+
+  }else{
+      
+  let todoList : Todo = {
+    name : this.name,
+    editable: this.editable
+  }
+   this.todos.splice(0,0,todoList)
+   this.name = "";
+
+  }
+ }
+
+ edit(item,index){
+  this.editable = true;
+  this.addButton = "Update";
+  this.name = item;
+  this.editableIndex = index;
+
+}
+
+  delete(index) {
+    this.todos.splice(index,1); 
+    alert("Deleted Value"); 
+     }
+
+
+     FieldsChange(values:any){
+      if(values.currentTarget.checked === true){
+      this.alert = true;
+     } else{
+       this.alert = false;
+     }
+    }
+  
 }
---
+++ src/app/todo.ts
@@ -0,0 +1,6 @@
+export class Todo {
+
+    name : string;
+    editable:Boolean;
+    
+}
